a203af4d5e9e65d37fbf48f7ac9b649a2b100ec7
Lucas Lollobrigida <lucaslollobrigida@gmail.com>
2019-08-22 14:59:41 -0300

    org: break the entries from the index file
    Make the entries of the radar a separated js file with a exported
    object. Remove unused files and update .gitignore.

---
+++ docs/entries.js
@@ -0,0 +1,279 @@
+function entries() {
+  return [
+    {
+      quadrant: 3,
+      ring: 0,
+      label: "AWS RDS",
+      active: true,
+      moved: 1
+    },
+    {
+      quadrant: 3,
+      ring: 0,
+      label: "PostgreSQL",
+      active: true,
+      moved: 0
+    },
+    {
+      quadrant: 3,
+      ring: 0,
+      label: "Redis",
+      active: true,
+      moved: 0
+    },
+    {
+      quadrant: 3,
+      ring: 0,
+      label: "AWS S3",
+      active: true,
+      moved: 1
+    },
+    {
+      quadrant: 3,
+      ring: 1,
+      label: "AWS ElastiCache",
+      active: true,
+      moved: 0
+    },
+    {
+      quadrant: 3,
+      ring: 1,
+      label: "SQLite",
+      active: true,
+      moved: 0
+    },
+    {
+      quadrant: 3,
+      ring: 2,
+      label: "MongoDB",
+      active: true,
+      moved: 0
+    },
+    {
+      quadrant: 3,
+      ring: 2,
+      label: "MongoDB Atlas",
+      active: true,
+      moved: 0
+    },
+    {
+      quadrant: 3,
+      ring: 3,
+      label: "Elasticsearch",
+      active: true,
+      moved: 0
+    },
+    {
+      quadrant: 3,
+      ring: 3,
+      label: "AWS DynamoDB",
+      active: true,
+      moved: 0
+    },
+    {
+      quadrant: 3,
+      ring: 3,
+      label: "Memcached",
+      active: true,
+      moved: 0
+    },
+    {
+      quadrant: 2,
+      ring: 0,
+      label: "Node.js",
+      active: true,
+      moved: 0
+    },
+    {
+      quadrant: 2,
+      ring: 0,
+      label: "React",
+      active: true,
+      moved: 0
+    },
+    {
+      quadrant: 2,
+      ring: 0,
+      label: "Express (Node.js)",
+      active: true,
+      moved: 0
+    },
+    {
+      quadrant: 2,
+      ring: 0,
+      label: "Redux",
+      active: true,
+      moved: 0
+    },
+    {
+      quadrant: 2,
+      ring: 1,
+      label: "Ant Design",
+      active: true,
+      moved: 1
+    },
+    {
+      quadrant: 2,
+      ring: 1,
+      label: "Echo (Go)",
+      active: true,
+      moved: 0
+    },
+    {
+      quadrant: 2,
+      ring: 2,
+      label: "d3.js",
+      active: true,
+      moved: 0
+    },
+    {
+      quadrant: 2,
+      ring: 3,
+      label: "Laravel",
+      active: true,
+      moved: 0
+    },
+    {
+      quadrant: 1,
+      ring: 0,
+      label: "AWS Beanstalk",
+      active: true,
+      moved: 1
+    },
+    {
+      quadrant: 1,
+      ring: 0,
+      label: "Jenkins",
+      active: true,
+      moved: 0
+    },
+    {
+      quadrant: 1,
+      ring: 0,
+      label: "Nginx",
+      active: true,
+      moved: 0
+    },
+    {
+      quadrant: 1,
+      ring: 1,
+      label: "Docker",
+      active: true,
+      moved: -1
+    },
+    {
+      quadrant: 1,
+      ring: 1,
+      label: "Prometheus",
+      active: true,
+      moved: 0
+    },
+    {
+      quadrant: 1,
+      ring: 1,
+      label: "Grafana",
+      active: true,
+      moved: 0
+    },
+    {
+      quadrant: 1,
+      ring: 1,
+      label: "Sentry",
+      active: true,
+      moved: 0
+    },
+    {
+      quadrant: 1,
+      ring: 1,
+      label: "LocalStack",
+      active: true,
+      moved: 0
+    },
+    {
+      quadrant: 1,
+      ring: 2,
+      label: "AWS Lambda",
+      active: true,
+      moved: 0
+    },
+    {
+      quadrant: 1,
+      ring: 2,
+      label: "Terraform",
+      active: true,
+      moved: 0
+    },
+    {
+      quadrant: 1,
+      ring: 2,
+      label: "Traefik",
+      active: true,
+      moved: 0
+    },
+    {
+      quadrant: 1,
+      ring: 3,
+      label: "Kubernetes",
+      active: true,
+      moved: -1
+    },
+    {
+      quadrant: 0,
+      ring: 0,
+      label: "Java",
+      active: true,
+      moved: 0
+    },
+    {
+      quadrant: 0,
+      ring: 0,
+      label: "JavaScript",
+      active: true,
+      link: "javascript.html",
+      moved: 0
+    },
+    {
+      quadrant: 0,
+      ring: 0,
+      label: "OpenAPI (Swagger)",
+      active: true,
+      link: "openapi_swagger.html",
+      moved: 1
+    },
+    {
+      quadrant: 0,
+      ring: 1,
+      label: "Go",
+      active: true,
+      moved: 1
+    },
+    {
+      quadrant: 0,
+      ring: 2,
+      label: "Python",
+      active: true,
+      moved: 0
+    },
+    {
+      quadrant: 0,
+      ring: 2,
+      label: "Kotlin",
+      active: true,
+      moved: 0
+    },
+    {
+      quadrant: 0,
+      ring: 3,
+      label: "TypeScript",
+      active: true,
+      moved: -1
+    },
+    {
+      quadrant: 0,
+      ring: 3,
+      label: "PHP",
+      active: true,
+      moved: 0
+    },
+  ];
+}
